e3b8e09676b04d93fef41d0b0251141557ebedd8
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-13 21:53:50 +0000

    Converted the store to use Immutable. Added a requestStories action.

---
+++ client/src/actions/index.js
@@ -0,0 +1,6 @@
+export const REQUEST_STORIES = "REQUEST_STORIES";
+export function requestStories() {
+    return {
+        type: REQUEST_STORIES
+    };
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,17 @@
-// A dummy reducer to get things going.
-function RootReducer(state = {}, action) {
+import Immutable, { Map, List } from "immutable";
+import { REQUEST_STORIES } from "../actions";
+
+// Construct an initial state object.
+const initialState = Immutable.fromJS({
+    stories: {}
+});
+
+function RootReducer(state = initialState, action) {
+    switch(action.type) {
+        case REQUEST_STORIES:
+            alert("Request for stories has been made");
+    } 
+    
     return state;
 }
 
