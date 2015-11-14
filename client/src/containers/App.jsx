7df9e8a78b58b4e20e0071d4faa36c0fe57383d5
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-14 15:55:55 +0000

    Added dispatcher bound action creators to tidy up the JSX.

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,14 +1,17 @@
 import React, { Component, PropTypes } from "react";
 import { Map } from "immutable";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { requestStories } from "../actions";
+import * as Actions from "../actions";
 import Stories from "../components/Stories";
 
 function App({dispatch, stories}) {
+    const {requestStories} = bindActionCreators(Actions, dispatch);
+    
     return (
         <div>
             <h1>Latest Hacker News Stories</h1>
-            <button onClick={() => dispatch(requestStories())}>
+            <button onClick={requestStories}>
                 Request Stories
             </button>
             <Stories stories={stories} />
