85873ec24d6f70f85b04e4f5fa3273f0bd7cf2d1
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-14 22:09:26 +0000

    Improved styling of the stories list. Stories now render their points.

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -3,13 +3,18 @@ import Story from "./Story";
 
 const styles = {
     listStyleType: "none",
+    padding: "0px 8px 8px 8px"
 };
 
 function Stories({stories}) {
     return (
         <ul style={styles}>
-            {stories.map((story) => (
-                 <Story key={story.get("id")} story={story} />
+            {stories.map((story, index) => (
+                 <Story
+                   key={story.get("id")}
+                   index={index}
+                   story={story}
+                 />
             ))}
         </ul>
     );
--- client/src/components/Story.jsx
+++ client/src/components/Story.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 
 const styles = {
-    borderTop: "1px solid black",
-    padding: "12px"
+    listItem: {
+        borderStyle: "solid",
+        borderColor: "#ddd",
+        borderWidth: "0px 1px 1px 1px",
+        backgroundColor: "#f3f3f3",
+        padding: "12px"
+    },
+    title: {
+        display: "block",
+        marginBottom: "6px",
+        color: "#666"
+    },
+    details: {
+        fontSize: "12px",
+        color: "#ccc"
+    },
+    link: {
+        color: "#ccc"
+    }
 }
 
-function Story({story}) {
-    const id = story.get("id");
-    const title = story.get("title");
-    const url = story.get("url");
-    
+function Story({story, index}) {
+    const listItemStyles = index ? styles.listItem : Object.assign({}, styles.listItem, {
+        borderTopWidth: "1px"
+    });
+
     return (
-        <li style={styles}>
-            <strong>{title}</strong>
-            <br />
-            <a href={url}>{url}</a>
+        <li style={listItemStyles}>
+            <strong style={styles.title}>{story.get("title")}</strong>
+            <div style={styles.details}>
+                <span>{story.get("score")} points</span>
+                <span> | </span>
+                <a style={styles.link} href={story.get("url")}>link</a>
+            </div>
         </li>
     );
 }
