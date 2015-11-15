ee05cc2bd700aaf99d13c16806684205b172b46e
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-15 00:49:10 +0000

    Store now handles FAILED_STORIES. Added new store prop: failedRequest.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,20 +1,30 @@
 import Immutable, { Map, List } from "immutable";
-import { REQUEST_STORIES, RECEIVE_STORIES } from "../actions";
+import { REQUEST_STORIES, RECEIVE_STORIES, FAILED_STORIES } from "../actions";
 
 // Construct an initial state object.
 const initialState = Immutable.fromJS({
     isFetching: false,
+    failedRequest: false,
     stories: []
 });
 
 function RootReducer(state = initialState, action) {
     switch(action.type) {
         case REQUEST_STORIES:
-            return state.set("isFetching", true);
+            return state
+                .set("isFetching", true)
+                .set("failedRequest", false);
+            
         case RECEIVE_STORIES:
             return state
                 .set("stories", Immutable.fromJS(action.stories))
-                .set("isFetching", false);
+                .set("isFetching", false)
+                .set("fetchError", false);
+            
+        case FAILED_STORIES:
+            return state
+                .set("isFetching", false)
+                .set("fetchError", true);
     }
     
     return state;
