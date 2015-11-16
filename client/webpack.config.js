8d2a7ee640dc1e12ace0b7fc699f3fd7f94c6bfe
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-16 21:38:12 +0000

    Added stage 2 presets (particularly for object reset/spread properties).

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (function() {
         test: /\.jsx?/,
         exclude: /node_modules/,
         query: {
-            presets: ["es2015", "react"]
+            presets: ["es2015", "stage-2", "react"]
         }
     });
 
