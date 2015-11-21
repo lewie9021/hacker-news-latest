ac0e7d9b560239bfc579d19f20492993550c3908
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-21 13:22:19 +0000

    Added a spec to ensure fetching stories works as expected.

--- client/specs/actions.js
+++ client/specs/actions.js
@@ -1,5 +1,6 @@
 import ConfigureStore from "redux-mock-store";
 import Thunk from "redux-thunk";
+import FetchMock from "fetch-mock";
 import * as actions from "../src/actions";
 
 const { expect } = window.chai;
@@ -7,11 +8,37 @@ const mockStore = ConfigureStore([
     Thunk
 ]);
 
-
 describe("Async Actions", function() {
-
+    
     it("should fetch stories", function(done) {
+        // Create a mock story.
+        const story = {
+            id: 1,
+            title: "Hello World",
+            url: "https://www.example.com",
+            by: "John Smith"
+        };
+
+        // Initial state of the store
+        const initialState = {};
+        // List out the actions we expect to fire (order matters).
+        const expectedActions = [
+            {type: actions.REQUEST_STORIES},
+            {type: actions.RECEIVE_STORIES, stories: [story]}
+        ];
+        // Construct our mock store.
+        const store = mockStore(initialState, expectedActions, done);
+
+        // Mock the server's /latest endpoint to return a pre-defined story.
+        FetchMock.mock("http://localhost:8080/latest", {
+            body: [story]
+        });
         
+        // Trigger the action!
+        store.dispatch(actions.fetchStories());
+
+        // Revert the mocking.
+        FetchMock.restore();
     });
     
 });
