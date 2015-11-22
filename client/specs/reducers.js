5e4ea260f3efe400be68e9397fa9ae0890a64d0d
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-22 19:56:25 +0000

    Added specs for the rest of the RootReducer. - Ensures REQUEST_STORIES, RECEIVE_STORIES, and FAILED_STORIES are handled correctly. - Getting initial state is now a function returning a new object each time it's called.

--- client/specs/reducers.js
+++ client/specs/reducers.js
@@ -4,21 +4,68 @@ import * as actions from "../src/actions";
 
 const { expect } = window.chai;
 
+function getInitialState() {
+    return Immutable.fromJS({
+        isFetching: false,
+        failedRequest: false,
+        stories: []
+    });
+}
+
 describe("Reducers", () => {
 
     describe("root", () => {
 
         it("should return the initial state", () => {
+            const initialState = getInitialState().toJS();
             const state = RootReducer(undefined, {});
-            const expected = {
-                isFetching: false,
-                failedRequest: false,
-                stories: []
-            };
 
             // Since we are dealing with Immutable data structures,
             // we need to convert it back to normal JS.
-            expect(state.toJS()).to.eql(expected);
+            expect(state.toJS()).to.eql(initialState);
+        });
+
+        it("should handle REQUEST_STORIES", () => {
+            const initialState = getInitialState();
+            const state = RootReducer(initialState, {
+                type: actions.REQUEST_STORIES
+            });
+            
+            expect(state.toJS()).to.eql({
+                ...initialState.toJS(),
+                isFetching: true,
+                failedRequest: false
+            });
+        });
+
+        it("should handle RECEIVE_STORIES", () => {
+            const initialState = getInitialState();
+            const story = {id: 1, title: "Hello World!"};
+            const state = RootReducer(initialState, {
+                type: actions.RECEIVE_STORIES,
+                stories: [story]
+            });
+
+            expect(state.toJS()).to.eql({
+                ...initialState.toJS(),
+                stories: [story],
+                isFetching: false,
+                fetchError: false
+            });
+        });
+
+        it("should handle FAILED_STORIES", () => {
+            const initialState = getInitialState();
+            const state = RootReducer(initialState, {
+                type: actions.FAILED_STORIES,
+                error: new Error("Request timed out.")
+            });
+
+            expect(state.toJS()).to.eql({
+                ...initialState.toJS(),
+                isFetching: false,
+                fetchError: true
+            });
         });
         
     });
