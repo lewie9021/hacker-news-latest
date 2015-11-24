22ab7590872446f28490d4e3751fca35a7d9e51a
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-24 21:17:25 +0000

    Added a helper that wraps an object's key values with Immutable.

--- client/specs/components/helpers.jsx
+++ client/specs/components/helpers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import TestUtils from "react-addons-test-utils";
+import TestUtils from "react-addons-test-utils"
+import Immutable from "immutable";
 
 export function renderComponent(Component, props) {
     const renderer = TestUtils.createRenderer();
@@ -13,3 +14,12 @@ export function renderComponent(Component, props) {
         output
     };
 }
+// Convert each property on 'props' to an Immutable data structure.
+export function convertPropsToImmutable(props) {
+    return Object.keys(props).reduce((map, prop) => {
+        map[prop] = Immutable.fromJS(props[prop]);
+        
+        return map;
+    }, {});
+}
+
