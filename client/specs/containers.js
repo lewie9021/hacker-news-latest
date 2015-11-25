fc20e06880e0ec7b1b062979e3ae47fe6f7464c4
Lewis Barnes <lb.barnes@live.co.uk>
2015-11-25 20:13:47 +0000

    Added skeleton specs for the App container.

---
+++ client/specs/containers.js
@@ -0,0 +1,36 @@
+import { App } from "../src/containers/App";
+
+const { expect } = window.chai;
+const Sinon = window.sinon;
+
+describe("Containers", () => {
+
+    describe("App", () => {
+
+        it("should render the Header component", () => {
+            
+        });
+
+        it("should provide a title prop for the Header component", () => {
+            
+        });
+
+        it("should set the Header component's 'actions' attribute to props.actions",  () => {
+            
+        });
+
+        it("should set the Header component's 'isFetching' attribute to props.isFetching", () => {
+            
+        });
+
+        it("should only render the Alert component when fetchError is truthy", () => {
+            
+        });
+
+        it("should render the Stories component, passing props.stories to its stories attribute", () => {
+            
+        });
+        
+    });
+    
+});
